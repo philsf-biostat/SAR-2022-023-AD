79ce6838b2f2f7c84815b61df8abf99f1d0edd31
Felipe Figueiredo <philsf79@gmail.com>
2022-05-03 20:52:21 -0300

    tables saved for performance boost

--- scripts/describe.R
+++ scripts/describe.R
@@ -27,20 +27,26 @@ theme_gtsummary_compact()
 
 # tables ------------------------------------------------------------------
 
-tab_desc <- analytical %>%
-  tbl_summary(
-    include = c(sex, age, pain_pre, koos_pre),
-    # by = group,
-    type = list(age ~ "continuous", pain_pre ~ "continuous", koos_pre ~ "continuous")
-  ) %>%
-  # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
-  # modify_header(label ~ "**Características dos pacientes**") %>%
-  bold_labels() %>%
-  modify_table_styling(columns = "label", align = "c")
+# tab_desc <- analytical %>%
+#   tbl_summary(
+#     include = c(sex, age, pain_pre, koos_pre),
+#     # by = group,
+#     type = list(age ~ "continuous", pain_pre ~ "continuous", koos_pre ~ "continuous")
+#   ) %>%
+#   # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
+#   modify_header(label ~ "**Baseline characteristics**") %>%
+#   bold_labels() %>%
+#   modify_table_styling(columns = "label", align = "c")
+# 
+# tab_outcome <- analytical %>%
+#   tbl_summary(
+#     include = c(outcome, pain_reduc, frequency),
+#     type = list(pain_reduc ~ "continuous", frequency = "categorical"),
+#     # by = frequency,
+#   )
+# 
+# write_rds(tab_desc, "dataset/tab_desc.rds")
+# write_rds(tab_outcome, "dataset/tab_outcome.rds")
 
-tab_outcome <- analytical %>%
-  tbl_summary(
-    include = c(outcome, pain_reduc, frequency),
-    type = list(pain_reduc ~ "continuous", frequency = "categorical"),
-    # by = frequency,
-  )
+tab_desc <- read_rds("dataset/tab_desc.rds")
+tab_outcome <- read_rds("dataset/tab_outcome.rds")
--- scripts/modeling.R
+++ scripts/modeling.R
@@ -27,23 +27,33 @@ m2_lab <- "Controlled for VAS reduction"
 m3_lab <- "Controlled for sex and age"
 m4_lab <- "Controlled for VAS reduction, sex and age"
 
-tab_inf <- tbl_stack(list(
-  tbl_regression(m0, include = frequency, show_single_row = frequency, label = list(frequency = m1_lab)),
-  tbl_regression(m1, include = frequency, show_single_row = frequency, label = list(frequency = m2_lab)),
-  tbl_regression(m2, include = frequency, show_single_row = frequency, label = list(frequency = m3_lab)),
-  tbl_regression(m3, include = frequency, show_single_row = frequency, label = list(frequency = m4_lab))
-  ),
-  )
+# create & save table objects ---------------------------------------------
 
-tab_app <- tbl_merge(list(
-  tbl_regression(m0),
-  tbl_regression(m1),
-  tbl_regression(m2),
-  tbl_regression(m3)
-  ),
-  c(
-    m1_lab,
-    m2_lab,
-    m3_lab,
-    m4_lab
-  ))
+# tab_inf <- tbl_stack(list(
+#   tbl_regression(m0, include = frequency, show_single_row = frequency, label = list(frequency = m1_lab)),
+#   tbl_regression(m1, include = frequency, show_single_row = frequency, label = list(frequency = m2_lab)),
+#   tbl_regression(m2, include = frequency, show_single_row = frequency, label = list(frequency = m3_lab)),
+#   tbl_regression(m3, include = frequency, show_single_row = frequency, label = list(frequency = m4_lab))
+#   ),
+#   )
+# 
+# tab_app <- tbl_merge(list(
+#   tbl_regression(m0),
+#   tbl_regression(m1),
+#   tbl_regression(m2),
+#   tbl_regression(m3)
+#   ),
+#   c(
+#     m1_lab,
+#     m2_lab,
+#     m3_lab,
+#     m4_lab
+#   ))
+# 
+# write_rds(tab_inf, file = "dataset/tab_mod.rds")
+# write_rds(tab_app, file = "dataset/tab_app.rds")
+
+# load table objects ------------------------------------------------------
+
+tab_inf <- read_rds(file = "dataset/tab_mod.rds")
+tab_app <- read_rds(file = "dataset/tab_app.rds")
